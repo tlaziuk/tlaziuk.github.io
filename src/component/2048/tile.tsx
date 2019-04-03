5fa9ed034da362e9d7f5cc7210daee6965749737
Tomek Łaziuk <tlaziuk@gmail.com>
2019-04-03 23:03:15 +0200

    tile styles

--- src/component/2048/tile.tsx
+++ src/component/2048/tile.tsx
@@ -1,10 +1,15 @@
 import {
     amber,
     blue,
+    brown,
+    grey,
+    indigo,
     lime,
     orange,
     pink,
     purple,
+    red,
+    teal,
     yellow,
 } from "@material-ui/core/colors";
 import Paper from "@material-ui/core/Paper";
@@ -17,10 +22,19 @@ interface IProps {
 
 export default withStyles(
     ({ typography }) => ({
-        tile: {
-            fontFamily: typography.fontFamily,
-            fontWeight: typography.fontWeightMedium,
+        svg: {
             height: "100%",
+            width: "100%",
+        },
+        text: {
+            fontFamily: typography.fontFamily,
+            fontSize: typography.fontSize,
+            fontWeight: typography.fontWeightMedium,
+        },
+        tile: {
+            display: "table",
+            height: "100%",
+            textAlign: "center",
             transitionDuration: "0.25s",
             transitionProperty: "all",
             transitionTimingFunction: "ease-in-out",
@@ -35,6 +49,8 @@ export default withStyles(
         const {
             classes: {
                 tile,
+                text,
+                svg,
             },
             value,
             theme: {
@@ -78,6 +94,26 @@ export default withStyles(
                 backgroundColor = pink.A100;
                 break;
             }
+            case 128: {
+                backgroundColor = brown.A100;
+                break;
+            }
+            case 256: {
+                backgroundColor = grey.A100;
+                break;
+            }
+            case 512: {
+                backgroundColor = teal.A100;
+                break;
+            }
+            case 1024: {
+                backgroundColor = indigo.A100;
+                break;
+            }
+            case 2048: {
+                backgroundColor = red.A100;
+                break;
+            }
             default: {
                 backgroundColor = paperBackground;
                 break;
@@ -86,7 +122,11 @@ export default withStyles(
 
         return <Paper
             className={tile}
-            style={{ backgroundColor, color: getContrastText(backgroundColor) }}
-        >{value > 0 ? value : undefined}</Paper>;
+            style={{ backgroundColor }}
+        >
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={svg}>
+                <text textAnchor="middle" x="12" y="17" className={text}>{value > 0 ? value : undefined}</text>
+            </svg>
+        </Paper>;
     }
 });
