1757c906eeaefda382ee72fcd7b2b8cb24438d25
Tomek Łaziuk <tlaziuk@gmail.com>
2019-06-15 19:26:53 +0200

    use-theme

--- src/root.tsx
+++ src/root.tsx
@@ -2,6 +2,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import React, { ComponentType, lazy, LazyExoticComponent, PureComponent, Suspense } from "react";
 import { Unsubscribable } from "rxjs";
+import ThemeColor from "./component/theme-color";
 import Progress from "./page/progress";
 import { resolvedRoute$ } from "./router";
 import theme from "./theme";
@@ -40,6 +41,7 @@ export default class RootContainer extends PureComponent<any, IState> {
         const { node: Node } = this.state;
         return <MuiThemeProvider theme={theme}>
             <CssBaseline />
+            <ThemeColor />
             <Suspense fallback={<Progress />}>
                 <Node />
             </Suspense>
