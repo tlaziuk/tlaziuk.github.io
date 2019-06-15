cd3befb31b067424005180ea43280f8aa74c38d0
Tomek Łaziuk <tlaziuk@gmail.com>
2019-06-15 18:11:28 +0200

    variadic pelette type

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,16 @@
+import { PaletteType } from "@material-ui/core";
 import { createMuiTheme } from "@material-ui/core/styles";
+
+let type: PaletteType;
+
+try {
+    type = matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark";
+} catch {
+    type = "dark";
+}
 
 export default createMuiTheme({
     palette: {
-        type: "dark",
+        type,
     },
 });
