31d0f524cd90bc7e849cf7a6a61604a923cc33ca
Tomek Łaziuk <tlaziuk@gmail.com>
2019-08-18 17:22:26 +0200

    konami function component

--- src/component/konami/index.tsx
+++ src/component/konami/index.tsx
@@ -1,96 +1,76 @@
-import { Component, createElement } from "react";
-import { fromEvent, Unsubscribable } from "rxjs";
-import { filter, map, scan } from "rxjs/operators";
+import { createElement, useEffect, useRef, useState } from "react";
 
 interface IProps {
-    code: ReadonlyArray<string>;
+    code?: ReadonlyArray<string>;
     action: () => any;
 }
 
-export default class Konami extends Component<IProps> {
-    public static readonly defaultProps: Partial<Readonly<IProps>> = {
-        code: [
-            "ArrowUp",
-            "ArrowUp",
-            "ArrowDown",
-            "ArrowDown",
-            "ArrowLeft",
-            "ArrowRight",
-            "ArrowLeft",
-            "ArrowRight",
-            "b",
-            "a",
-        ],
-    };
+const defaultCode: Required<IProps>["code"] = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+];
 
-    private document?: Document;
+const defaultKeys = [] as ReadonlyArray<string>;
 
-    private subscription?: Unsubscribable;
+export default function Konami({
+    code = defaultCode,
+    action,
+}: IProps) {
+    const ref = useRef<HTMLDivElement>(null);
+    const [keys, setKeys] = useState(defaultKeys);
 
-    public shouldComponentUpdate() {
-        return false;
-    }
-
-    public componentWillUnmount() {
-        const { subscription } = this;
-        if (subscription) {
-            subscription.unsubscribe();
-        }
-    }
-
-    public render() {
-        return createElement(
-            "div",
-            {
-                ref: this.handleRef,
-                style: { display: "none" },
-            },
-        );
-    }
-
-    private readonly handleRef = (element: HTMLElement | null) => {
-        if (element) {
-            const document = element.ownerDocument;
-            if (document && document !== this.document) {
-                this.document = document;
-                const { subscription } = this;
-                if (subscription) {
-                    subscription.unsubscribe();
-                }
-                this.subscription = fromEvent<KeyboardEvent>(document, "keyup").pipe(
-                    map(({ key }) => key),
-                    scan<string, ReadonlyArray<string>>(
-                        (accumulator, value) => {
-                            const { code } = this.props;
-                            return [
-                                ...accumulator.slice(
-                                    accumulator.length < code.length ? 0 : 1,
-                                    code.length + 1,
-                                ),
-                                value,
-                            ];
-                        },
-                        [],
-                    ),
-                    filter(
-                        (value) => {
-                            const { code } = this.props;
-                            for (let i = 0; i < code.length; i++) {
-                                if (value[i] !== code[i]) {
-                                    return false;
-                                }
-                            }
-                            return true;
-                        },
-                    ),
-                ).subscribe(async () => {
-                    try {
-                        await this.props.action();
-                    } catch {
-                        // pass
-                    }
-                });
+    useEffect(
+        () => {
+            const { current: element } = ref;
+            if (!element) {
+                return;
             }
-        }
-    }
+
+            const { ownerDocument: document } = element;
+            if (!document) {
+                return;
+            }
+
+            const listener = ({ key }: KeyboardEvent) => {
+                setKeys((stateKeys) => [key, ...stateKeys]);
+            };
+
+            document.addEventListener("keyup", listener);
+
+            return () => {
+                document.removeEventListener("keyup", listener);
+            };
+        },
+        [ref.current && ref.current.ownerDocument],
+    );
+
+    useEffect(
+        () => {
+            const keysTmp = keys.slice(0, code.length).reverse();
+
+            for (let index = 0; index < code.length; index++) {
+                if (code[index] !== keysTmp[index]) {
+                    return;
+                }
+            }
+            action();
+        },
+        [keys, code, action],
+    );
+
+    return createElement(
+        "div",
+        {
+            ref,
+            style: { display: "none" },
+        },
+    );
 }
